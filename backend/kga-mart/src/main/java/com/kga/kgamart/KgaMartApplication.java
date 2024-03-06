63c9ddc8c7ab2d7309c30e0d1a97b55e6d90abaf
Gyanendra Samdarshi <gyanendra_samdarshi@persistent.com>
2024-03-06 10:59:28 +0530

    initial commit-created directory structure

---
+++ backend/kga-mart/src/main/java/com/kga/kgamart/KgaMartApplication.java
@@ -0,0 +1,13 @@
+package com.kga.kgamart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgaMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgaMartApplication.class, args);
+	}
+
+}
---
+++ backend/kga-mart/src/test/java/com/kga/kgamart/KgaMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kga.kgamart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgaMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
